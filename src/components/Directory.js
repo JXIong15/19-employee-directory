c7ca20df423bf0f4fdeb041628a4c739d316d38f
Pikachu <jouxiong15@gmail.com>
2021-06-17 01:10:00 -0500

    search button works

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -5,16 +5,32 @@ class Directory extends React.Component {
         empArr: [],
         sortEmp: [],
         search: "",
-        sorted:false
+        sorted: false
     };
 
-    handleSearch = () => {
-        
+    handleSearch = (event) => {
+        event.preventDefault();
+        console.log("Search");
     }
 
     render() {
         return (
-            <h1>TEST</h1>
+            <div className="search">
+                <h1>Employee Directory</h1>
+                <form>
+                    <p>Search for an employee by name or email.</p>
+                    <input type="text" placeholder="Search for Employee" />
+                    <input type="submit" value="Search" onClick={this.handleSearch} />
+
+                </form>
+
+                <div className="emp-list">
+                    <ul>
+                        <li>emp list</li>
+                    </ul>
+                </div>
+
+            </div>
         )
     }
 }
