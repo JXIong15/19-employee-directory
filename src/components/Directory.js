8bf92bbab659d00f446a00ab3c89ff6ef2b51a3a
Pikachu <jouxiong15@gmail.com>
2021-06-16 23:29:40 -0500

    added components and edited app.js

---
+++ src/components/Directory.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Directory extends React.Component {
+    state = {
+        empArr: [],
+        sortEmp: [],
+        search: "",
+        sorted:false
+    }
+}
+
+export default Directory;
